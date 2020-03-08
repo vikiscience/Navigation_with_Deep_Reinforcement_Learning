658e5fe3f66a05b863a9487195e061ed0de5a9a3
vikiscience <victoria.bibaeva@yandex.com>
2020-03-08 20:58:43 +0100

    use argparse and refactored Env

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 import const
-from src import agent, algo, ref, hyperparameter_search, utils_env
+from src import agent, algo, hyperparameter_search, utils_env
 
 import numpy as np
+import argparse
 
 random_seed = const.random_seed
 np.random.seed(random_seed)
 
-#state_size, action_size = utils_env.get_info()
 state_size = const.state_size
 action_size = const.action_size
 N = const.rolling_mean_N
 
 
 def train_default_algo():
+    env = utils_env.Environment()
     # use default params
     ag = agent.DQNAgent()
-    # ag = ref.Agent()
-    al = algo.DQNAlgo(utils_env.env, ag)
+    al = algo.DQNAlgo(env, ag)
     al.train()
 
 
-def test_default_algo():
+def test_default_algo(use_ref_model: bool = False):
+    env = utils_env.Environment()
     # use default params
     ag = agent.DQNAgent()
-    # ag = ref.Agent()
-    al = algo.DQNAlgo(utils_env.env, ag)
+    if use_ref_model:
+        print('... Test the agent using reference model ...')
+        ag.set_model_path('ref')
+    al = algo.DQNAlgo(env, ag)
     al.test()
 
 
 if __name__ == '__main__':
-    #train_default_algo()
-    #test_default_algo()
+    parser = argparse.ArgumentParser(description='Train and test a Deep '
+                                                 'Reinforcement Learning agent '
+                                                 'to navigate in a Banana Environment',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--exec', choices=['train', 'test', 'grid'],
+                        default='train', help='Train/test a DQN agent with default parameters or perform grid search to find the best agent')
+    parser.add_argument('-r', '--use_reference_model', action="store_true", default=False,
+                        help='In Test Mode, use the pretrained reference model')
 
-    hyperparameter_search.grid_search()
+    args = parser.parse_args()
+    exec = args.exec
+    use_ref_model = args.use_reference_model
 
-    #hist = [0.0, 4.0, 0.0, 1.0, -3.0, 0.0, 2.0, 0.0, 0.0, 0.0, 1.0, 1.0, -1.0, 0.0, -1.0, 3.0, 1.0, 0.0, 2.0, -3.0, 1.0, -1.0, 0.0, 0.0, 2.0, -2.0, 5.0, 2.0, 0.0, 0.0, 0.0, -2.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, -5.0, 2.0, 0.0, 0.0, 2.0, -1.0, -2.0, 1.0, 1.0, 0.0, -2.0, -2.0, 0.0, -1.0, 2.0, 1.0, 1.0, 0.0, 2.0, -1.0, -2.0, 1.0, 0.0, 0.0, -3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 1.0, 0.0, 1.0, -1.0, 1.0, 1.0, 2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 1.0, 0.0, 2.0, -2.0, 0.0, 0.0, -1.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -3.0, 0.0, -2.0, 2.0, -3.0, 0.0, 1.0, 1.0, 1.0, -1.0, 0.0, 1.0, 2.0, -1.0, 1.0, 0.0, 0.0, 0.0, -2.0, -1.0, -1.0, 0.0, 1.0, 5.0, 1.0, 0.0, -1.0, -1.0, 0.0, 0.0, 2.0, -3.0, 0.0, 0.0, -3.0, 2.0, 3.0, -2.0, -2.0, 1.0, -1.0, 0.0, -1.0, -3.0, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, -2.0, -3.0, -1.0, -2.0, 0.0, -2.0, 2.0, -2.0, -2.0, 0.0, 0.0, 1.0, -3.0, 3.0, 0.0, 1.0, -3.0, -2.0, -1.0, 1.0, 2.0, 1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0, -1.0, -1.0, -1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 3.0, -3.0, 1.0, 1.0, -2.0, -3.0, -1.0, -1.0, 2.0, 0.0, -3.0, -2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 1.0, 0.0, -1.0, 0.0, 0.0, -2.0, -1.0, -1.0, 0.0, -1.0, -2.0, 1.0, 2.0, 1.0, 0.0, 2.0, 1.0, 1.0, -1.0, 0.0, 1.0, -2.0, 4.0, -1.0, 1.0, -1.0, -1.0, 1.0, 2.0, 0.0, 2.0, 2.0, 0.0, 0.0, 1.0, 0.0, 0.0, -3.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -3.0, 2.0, -2.0, -2.0, 0.0, 0.0, 2.0, 0.0, 0.0, -2.0, 1.0, -2.0, 1.0, 3.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0, 2.0, 0.0, 1.0, 0.0, 2.0, -1.0, 0.0, -1.0, 0.0, -1.0, -3.0, 0.0, 2.0, 1.0, 3.0, -1.0, 2.0, -1.0, 0.0, 1.0, 0.0, -3.0, 0.0, 0.0, 0.0, -2.0, -4.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, -1.0, 1.0, 2.0, -2.0, 1.0, 0.0, -1.0, 3.0, 2.0, -1.0, 0.0, 3.0, 1.0, 3.0, 0.0, 3.0, 0.0, 0.0, 0.0, 2.0, 0.0, -1.0, -1.0, 1.0, -2.0, -1.0, 0.0, 0.0, -1.0, 3.0, 1.0, 0.0, -2.0, 1.0, -1.0, 0.0, -1.0, 2.0, -2.0, 0.0, 1.0, 0.0, -1.0, 2.0, 0.0, 1.0, 1.0, 0.0, 1.0, -1.0, -2.0, -2.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.0, 2.0, 1.0, 0.0, 3.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, -2.0, 1.0, 1.0, 0.0, 0.0, 1.0, 2.0, -1.0, -1.0, 0.0, 3.0, -1.0, -1.0, -1.0, 2.0, -1.0, 0.0, 2.0, 0.0, 1.0, 0.0, 1.0, -2.0, -2.0, -2.0, 0.0, -1.0, 1.0, 1.0, 1.0, -2.0, 1.0, -3.0, -3.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, -1.0, -1.0, -1.0, -1.0, 0.0, 0.0, 1.0, -2.0, 1.0, 1.0, -1.0, -1.0, 1.0, 2.0, 0.0, -2.0, 2.0, -1.0, -1.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 2.0, -2.0, 0.0, 0.0, 0.0, -1.0, -1.0, -3.0, -3.0, 0.0, 0.0, 0.0, 0.0, 1.0, -2.0, 1.0, 2.0, -3.0, 2.0, 0.0, 1.0, -3.0, -2.0, 1.0, -1.0, 1.0, 0.0, -2.0, 0.0, 1.0, 0.0, -4.0, 3.0, -3.0, 1.0, -1.0, 1.0, -1.0, -2.0, -1.0, 2.0, -1.0]
-    #from src import utils_plot
-    #utils_plot.plot_history_rolling_mean(hist)
+    if exec == 'train':
+        train_default_algo()
+    elif exec == 'test':
+        test_default_algo(use_ref_model)
+    else:
+        hyperparameter_search.grid_search()
