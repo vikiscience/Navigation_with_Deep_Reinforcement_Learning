6eb96502f73cd08142031dc8a475da159ee857fd
vikiscience <victoria.bibaeva@yandex.com>
2020-03-08 20:42:02 +0100

    refactored utils_env

--- src/utils_env.py
+++ src/utils_env.py
@@ -2,28 +2,30 @@ import const
 
 from unityagents import UnityEnvironment
 
-env = UnityEnvironment(file_name=const.file_name_env)
 
+class Environment(UnityEnvironment):
+    def __init__(self):
+        super().__init__(file_name=const.file_name_env)
 
-def get_info():
-    # get the default brain
-    brain_name = env.brain_names[0]
-    brain = env.brains[brain_name]
+    def get_info(self):
+        # get the default brain
+        brain_name = self.brain_names[0]
+        brain = self.brains[brain_name]
 
-    # reset the environment
-    env_info = env.reset(train_mode=True)[brain_name]
+        # reset the environment
+        env_info = self.reset(train_mode=True)[brain_name]
 
-    # number of agents in the environment
-    print('Number of agents:', len(env_info.agents))
+        # number of agents in the environment
+        print('Number of agents:', len(env_info.agents))
 
-    # number of actions
-    action_size = brain.vector_action_space_size
-    print('Number of actions:', action_size)
+        # number of actions
+        action_size = brain.vector_action_space_size
+        print('Number of actions:', action_size)
 
-    # examine the state space
-    state = env_info.vector_observations[0]
-    print('States look like:', state)
-    state_size = len(state)
-    print('States have length:', state_size)
+        # examine the state space
+        state = env_info.vector_observations[0]
+        print('States look like:', state)
+        state_size = len(state)
+        print('States have length:', state_size)
 
-    return state_size, action_size
+        return state_size, action_size
